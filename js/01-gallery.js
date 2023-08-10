94b1a27c83a45e334fd0569de848df2b365a6d99
Emilia Wenta <emiliakielinska96@gmail.com>
2023-08-10 14:13:06 +0200

    Wykonanie połowy zadania 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,9 +3,28 @@ import { galleryItems } from "./gallery-items.js";
 
 const gallery = document.querySelector(".gallery");
 
-for (galleryItem of galleryItems) {
-  gallery.insertAdjacentHTML(
-    "beforeend",
-    `<li><img src= ${galleryItem.preview} alt= "${galleryItem.description}" ></img></li>`
-  );
+const galleryItemImage = document.querySelector(".gallery__image");
+
+galleryItems.forEach(function (galleryItem) {
+  const galleryItemDiv = document.createElement("div");
+  galleryItemDiv.classList.add("gallery__item");
+  gallery.append(galleryItemDiv);
+  const galleryItemLink = document.createElement("a");
+  galleryItemLink.classList.add("gallery__link");
+  galleryItemLink.href = `${galleryItem.original}`;
+  galleryItemDiv.append(galleryItemLink);
+  const galleryItemImage = document.createElement("img");
+  galleryItemImage.classList.add("gallery__image");
+  galleryItemImage.src = `${galleryItem.preview}`;
+  galleryItemImage.alt = `${galleryItem.description}`;
+  galleryItemImage.setAttribute("data-source", `${galleryItem.original}`);
+  galleryItemLink.append(galleryItemImage);
+});
+
+const galleryItemLinkAll = document.querySelectorAll(".gallery__link");
+
+for (const galleryItemLink of galleryItemLinkAll) {
+  galleryItemLink.addEventListener("click", (event) => {
+    event.preventDefault();
+  });
 }
