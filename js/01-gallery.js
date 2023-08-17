703fd15b054b82343a4b6165aad20fef36c1a16c
Emilia Wenta <emiliakielinska96@gmail.com>
2023-08-17 12:04:22 +0200

    Poprawki nr 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,10 @@
 import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
 const gallery = document.querySelector(".gallery");
+
+const newElements = [];
 
 galleryItems.forEach(function (galleryItem) {
   const galleryItemDiv = document.createElement("div");
@@ -9,7 +12,7 @@ galleryItems.forEach(function (galleryItem) {
   gallery.append(galleryItemDiv);
   const galleryItemLink = document.createElement("a");
   galleryItemLink.classList.add("gallery__link");
-  galleryItemLink.href = `${galleryItem.original}`;
+  galleryItemLink.href = `${galleryItem.preview}`;
   galleryItemDiv.append(galleryItemLink);
   const galleryItemImage = document.createElement("img");
   galleryItemImage.classList.add("gallery__image");
@@ -17,28 +20,24 @@ galleryItems.forEach(function (galleryItem) {
   galleryItemImage.alt = `${galleryItem.description}`;
   galleryItemImage.setAttribute("data-source", `${galleryItem.original}`);
   galleryItemLink.append(galleryItemImage);
+  newElements.push(galleryItemDiv);
 });
 
-const galleryItemLinkAll = document.querySelectorAll(".gallery__link");
+gallery.append(...newElements);
 
-for (const galleryItemLink of galleryItemLinkAll) {
-  galleryItemLink.addEventListener("click", (event) => {
-    event.preventDefault();
-  });
-}
+gallery.addEventListener("click", selectedImage);
 
-const galleryItemDivAll = document.querySelectorAll(".gallery__item");
+function selectedImage(event) {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  event.preventDefault();
 
-const imageOriginal = galleryItems.map((galleryItem) => galleryItem.original);
+  const image = event.target.dataset.source;
 
-for (let i = 0; i < galleryItemDivAll.length; i++) {
-  galleryItemDivAll[i].onclick = () => {
-    basicLightbox
-      .create(
-        `
-        <img width="1400" height="900" src="${imageOriginal[i]}">
-      `
-      )
-      .show();
-  };
+  const instance = basicLightbox.create(`
+      <img src="${image}" width="800" height="600">
+   `);
+
+  instance.show();
 }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -5,12 +5,14 @@ console.log(galleryItems);
 
 const gallery = document.querySelector(".gallery");
 
+const newElements = [];
+
 galleryItems.forEach(function (galleryItem) {
   const galleryItemLi = document.createElement("li");
   gallery.append(galleryItemLi);
   const galleryItemLink = document.createElement("a");
   galleryItemLink.classList.add("gallery__item");
-  galleryItemLink.href = `${galleryItem.original}`;
+  galleryItemLink.href = `${galleryItem.preview}`;
   galleryItemLi.append(galleryItemLink);
   const galleryItemImage = document.createElement("img");
   galleryItemImage.classList.add("gallery__image");
@@ -19,14 +21,18 @@ galleryItems.forEach(function (galleryItem) {
   galleryItemImage.style.display = "block";
   galleryItemImage.style.height = "100%";
   galleryItemLink.append(galleryItemImage);
+  newElements.push(galleryItemLi);
 });
 
-const galleryItemLinkAll = document.querySelectorAll(".gallery__item");
+gallery.append(...newElements);
 
-for (const galleryItemLink of galleryItemLinkAll) {
-  galleryItemLink.addEventListener("click", (event) => {
-    event.preventDefault();
-  });
+gallery.addEventListener("click", selectedImage);
+
+function selectedImage(event) {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  event.preventDefault();
 }
 
 const lightbox = new SimpleLightbox(".gallery a", {
